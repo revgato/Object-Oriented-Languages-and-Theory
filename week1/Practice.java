345cf05ca3679ba019e501b32ac93bcf08ae3e86
revgato <locchoihay147@gmail.com>
2022-04-26 07:51:01 +0700

    No commit

---
+++ week1/Practice.java
@@ -0,0 +1,16 @@
+public class Practice {
+    public static void main(String[] args){
+        String name;
+        float height;
+        int age;
+        name = "Chau Kter";
+        height = 1.7f;
+        age = 21;
+        System.out.print("Ten: ");
+        System.out.println(name);
+        System.out.print("Chieu cao:");
+        System.out.println(height);
+        System.out.print("Tuoi: ");
+        System.out.println(age);
+    }
+}
---
+++ week3/AimsProject/Order.java
@@ -0,0 +1,34 @@
+//Bai 5
+public class Order {
+    public static final int MAX_NUMBERS_ORDERED = 10;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered;
+    public void addDigitalVideoDisc(DigitalVideoDisc disc){
+        if(qtyOrdered == MAX_NUMBERS_ORDERED){
+            System.out.println("The order is almost full");
+            return;
+        }
+        itemsOrdered[qtyOrdered++] = disc;
+        System.out.println("The disc has been added");
+    }
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc){
+        for(int i = 0; i<qtyOrdered;i++){
+            if(itemsOrdered[i] == disc) {
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+                qtyOrdered--;
+                System.out.println("The disc has been removed");
+                return;
+            }
+        }
+        System.out.println("Cannot find this disc");
+    }
+     public float totalCost(){
+        float totalCost = 0;
+        for(int i=0;i<qtyOrdered;i++){
+            totalCost += itemsOrdered[i].getCost();
+        }
+        return totalCost;
+     }
+}
